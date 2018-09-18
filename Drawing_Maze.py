19356b3455850b97ed9cf0c9ee75e48db84eab12
Freddie Payne <fp17698@my.bristol.ac.uk>
2018-09-18 11:10:21 +0100

    Updated in line with changes to graph storage

--- Drawing_Maze.py
+++ Drawing_Maze.py
@@ -8,42 +8,45 @@ pi = math.pi
 
 from Objects import *
 
-#making a complement graph with respect to a complete graph - used for drawing the maze
-def graphComplement(graph, completeGraph):
-    complement = []
-    for edge in completeGraph:
-        if edge not in graph:
-            complement.append(edge)
-    return complement
-
 #define a rotation of a vertex about a given centre - used for maze drawing
-def rot(theta,vertex,centre):
+def rot(theta, vertex, centre):
     centeredv = vertex - centre
     rotatedv = v(centeredv[0]*math.cos(theta) - centeredv[1]*math.sin(theta), centeredv[0]*math.sin(theta) + centeredv[1]*math.cos(theta))
     finalv = rotatedv + centre
     return finalv
 
 #drawing the maze itself
-def drawMaze(l,w,graph,graphComplete,s,gameDisplay,surface):
+def drawMaze(graph, s, gameDisplay, surface):
   
-    rect = pygame.Rect((0,0),(s*l,s*w)) #outline of maze
-    pygame.draw.rect(surface,(200,200,200,1),rect,5)
+    # length and width of maze
+    l = graph.breadth
+    w = graph.width
 
-    complement = graphComplement(graph, graphComplete)
+    rect = pygame.Rect((0,0), (s*l,s*w)) #outline of maze
+    pygame.draw.rect(surface, (200, 200, 200, 1), rect, 5)
+
+    # Get complement graph 
+    complement = getComplement(graph)
 
 #drawing the walls
 #rotate each edge of the complement graph by pi/2
 #this makes the absence of an edge in the maze-tree be drawn as a wall
-    for edge in complement:
-        rotv1 = rot(pi/2,edge[0],centre(edge))
-        rotv2 = rot(pi/2,edge[1],centre(edge))
-        if (edge[0][0] == edge[1][0]) or (edge[0][1] == edge[1][1]):
-            pygame.draw.line(surface,(200,200,200,1),(rotv1[0],rotv1[1]),(rotv2[0],rotv2[1]),2)
+    for i in range(graph.size): 
+        for j in range(i):
+            # reference i and reference j are connected just in case adjacencyMatrix[i][j] = 1
+            if complement.adjacencyMatrix[i][j] == 1: 
+                # Get vertex coords and add relevant edge 
+                v1 = convertRefToCoords(graph, i, s)
+                v2 = convertRefToCoords(graph, j, s)
+                edgeToAdd = edge(v1, v2)
+                # Rotate vertices about centre of edgeToAdd
+                rotv1 = rot(pi/2, v1, centre(edge))
+                rotv2 = rot(pi/2, v2, centre(edge))
+                # Draw the edge onto the screen 
+                pygame.draw.line(surface, (200, 200, 200, 1), (rotv1[0], rotv1[1]), (rotv2[0], rotv2[1]), 2 )
 
-#drawing the finish square
+    #drawing the finish square
     pygame.draw.rect(surface,(0,200,0,1),((s*(l-0.75),s*(w-0.75)),(s/2,s/2)),0)
     pygame.display.flip()
- 
-        
-
- 
+    
+    return
