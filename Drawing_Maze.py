cbc0423d923278ef45d2806acccf63574cf456fe
Freddie Payne <fp17698@my.bristol.ac.uk>
2018-09-24 16:44:40 +0100

    Debugging
    Debugged fully. Passed testing.

--- Drawing_Maze.py
+++ Drawing_Maze.py
@@ -25,27 +25,27 @@ def drawMaze(graph, s, gameDisplay, surface):
     rect = pygame.Rect((0,0), (s*l,s*w)) #outline of maze
     pygame.draw.rect(surface, (200, 200, 200, 1), rect, 5)
 
-    # Get complement graph 
-    complement = getComplement(graph)
-
 #drawing the walls
 #rotate each edge of the complement graph by pi/2
 #this makes the absence of an edge in the maze-tree be drawn as a wall
     for i in range(graph.size): 
         for j in range(i):
-            # reference i and reference j are connected just in case adjacencyMatrix[i][j] = 1
-            if ( complement.adjacencyMatrix[i][j] == 1 ) and ( i != j ): 
+            # reference i and reference j are disconnected just in case adjacencyMatrix[i][j] = 0
+            if ( graph.adjacencyMatrix[i][j] == 0 ): 
                 # Get vertex coords and add relevant edge 
                 [v1_x, v1_y] = convertRefToCoords(graph, i+1, s)
                 [v2_x, v2_y] = convertRefToCoords(graph, j+1, s)
-                v1 = v(v1_x, v2_x, s)
+                v1 = v(v1_x, v1_y, s)
                 v2 = v(v2_x, v2_y, s)
-                edgeToAdd = edge(v1, v2)
-                # Rotate vertices about centre of edgeToAdd
-                rotv1 = rot(pi/2, v1, centre(edgeToAdd))
-                rotv2 = rot(pi/2, v2, centre(edgeToAdd))
-                # Draw the edge onto the screen 
-                pygame.draw.line(surface, (200, 200, 200, 1), (rotv1[0], rotv1[1]), (rotv2[0], rotv2[1]), 2 )
+                
+                # Now test if vertices are adjacent on graph
+                if adjacentTest(graph, v1, v2): 
+                    edgeToAdd = edge(v1, v2)
+                    # Rotate vertices about centre of edgeToAdd
+                    rotv1 = rot(pi/2, v1, centre(edgeToAdd))
+                    rotv2 = rot(pi/2, v2, centre(edgeToAdd))
+                    # Draw the edge onto the screen 
+                    pygame.draw.line(surface, (200, 200, 200, 1), (rotv1[0], rotv1[1]), (rotv2[0], rotv2[1]), 2 )
 
     #drawing the finish square
     pygame.draw.rect(surface,(0,200,0,1),((s*(l-0.75),s*(w-0.75)),(s/2,s/2)),0)
